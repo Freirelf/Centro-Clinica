06f31de64f66157b8a1ea94d1c5bfc7377fadae2
= <lucas.freire61@aluno.ifce.edu.br>
2023-04-12 19:42:57 -0300

    updates

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ function openMenu() {
   document.body.classList.add('menu-expended')
 }
 
-function closeMenu(){
+function closeMenu() {
   document.body.classList.remove('menu-expended')
+}
+
+function cardOpen (){
+  const openCard = document.querySelectorAll('.card');
+  
+  openCard.forEach(function(open) {
+    open.addEventListener('click', function() {
+      open.classList.add('card-expended');
+    });
+
+  });
+}
+
+function closeCard() {
+  const closeCard = document.querySelectorAll('.card')
+
+  closeCard.forEach(function(close) {
+    close.addEventListener('click', function() {
+      close.classList.remove('card-expended')
+    })
+  })
 }
